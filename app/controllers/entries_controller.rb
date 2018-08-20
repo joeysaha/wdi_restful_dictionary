9ce87a385350225b5a6e047e4adb92b116805013
Joey Saha <joeysaha@hotmail.com>
2018-08-20 05:00:46 -0400

    final up for part 2, setting up the views

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,29 +1,52 @@
 class EntriesController < ApplicationController
   def index
+    # here we'll define some @instance_variables to store data from the database for the views to use
+    @entries = Entry.all
     render :index
   end
 
   def show
+    @entry = Entry.find(params[:id])
     render :show
   end
 
   def new
+    @entry = Entry.new
     render :new
   end
 
   def create
-    redirect_to entries_url
+    @entry = Entry.new
+    @entry.word = params[:entry][:word]
+    @entry.language = params[:entry][:language]
+    @entry.definition = params[:entry][:definition]
+    if @entry.save
+      redirect_to entries_url
+    else
+      render :new
+    end
   end
 
   def edit
+    @entry = Entry.find(params[:id])
     render :edit
   end
 
   def update
-    redirect_to entry_url(params[:id])
+    @entry = Entry.find(params[:id])
+    @entry.word = params[:entry][:word]
+    @entry.language = params[:entry][:language]
+    @entry.definition = params[:entry][:definition]
+    if @entry.save
+      redirect_to entry_url(params[:id])
+    else
+      render :new
+    end
   end
 
   def destroy
-    redirect_to index_url
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+    redirect_to entries_path
   end
 end
